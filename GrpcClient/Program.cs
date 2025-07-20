0f38b019d8271218427da022864f4e9a8a2edb42
Tim Stelzig <tim.stelzig@gmx.de>
2025-07-20 18:24:55 +0200

    initial ms learn example

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5299");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest{Name = "Test"});
+
+Console.WriteLine("Greeting: " + reply.Message);
